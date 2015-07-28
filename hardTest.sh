22583c13636d464130cf2d9bdcb417a2b16fc1f2
Boste <boste09@student.sdu.dk>
2015-07-28 13:52:34 +0200

    removed batch files

--- hardTest.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cat testInstances.txt | while read line
-do
-	PARA=" ../../data/"$line	
-	./solver-v2/src/xyz $PARA
-done
-#	PARAMETERS="--main::method TS --main::output_file outTest.txt --main::instance "$line
-#	./xyz $PARAMETERS
-	 
